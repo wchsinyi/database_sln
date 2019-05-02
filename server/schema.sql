3371ee901887957e1a02ada918a54d03879e5473
Fred Zirdung <fred@hackreactor.com>
2019-05-02 12:30:15 -0700

    Solution Code

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+
+  id int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  text varchar(200)  NOT NULL,
+  roomname varchar(20),
+  PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
 
 
+CREATE TABLE users (
+  id        int    NOT NULL AUTO_INCREMENT,
+  username  varchar(40)   NOT NULL,
+  PRIMARY KEY (ID)
+);
 
 
 /*  Execute this file from the command line by typing:
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,8 +16,9 @@ describe('Persistent Node Chat Server', function() {
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
 
+    var tablename = 'messages';
+    
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
     dbConnection.query('truncate ' + tablename, done);
@@ -67,9 +68,10 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-       var queryString = "";
-       var queryArgs = [];
-    // TODO - The exact query string and query args to use
+
+    var queryString = 'INSERT INTO messages(text, userid, roomname) VALUES (?, ?, ?)';
+    var queryArgs = ['Men like you can never change!', 1, 'main'];
+        // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
 
